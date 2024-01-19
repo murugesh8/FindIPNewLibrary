2752175c06204280798e787a11a34cc954313606
Murugesh <murugesh.sm10@gmail.com>
2024-01-19 22:55:20 +0530

    View as changed based on appstate

--- Sources/FindMyIPLibrary/GetIPViewModel.swift
+++ Sources/FindMyIPLibrary/GetIPViewModel.swift
@@ -8,9 +8,19 @@
 import Foundation
 import Combine
 
+
+enum AppState{
+    case initial
+    case loading
+    case Sucess
+    case failure
+    
+}
 class GetIPViewModel : ObservableObject{
     
     @Published var getIpResponse:GetIPAPIResponse?
+    
+    @Published var state:AppState = .initial
     
     var networking: NetworkProtocol?
     var subscriptions : Set<AnyCancellable> = []
@@ -22,15 +32,21 @@ class GetIPViewModel : ObservableObject{
     }
     
     func getIpApi(){
-        
+        self.state = .loading
         networking?.getIpAPI()
-            .sink(receiveCompletion: { result in
-                print(result)
+            .sink(receiveCompletion: {[weak self] result in
+                guard self != nil else {
+                    self?.state = .failure
+                    return
+                }
+                
             }, receiveValue: { response in
                 self.getIpResponse = response
-                
+                self.state = .Sucess
+
             })
             .store(in: &subscriptions)
         
     }
 }
+
--- Sources/FindMyIPLibrary/View/FindMyIPView.swift
+++ Sources/FindMyIPLibrary/View/FindMyIPView.swift
@@ -12,7 +12,9 @@ public struct FindMyIPView: View {
     
     @StateObject var getIpViewModel : GetIPViewModel = GetIPViewModel(networking: NetworkManager())
     
-    public init() {
+    @State var isButtonTapped: Bool = false
+    
+    init() {
         
     }
     
@@ -20,59 +22,111 @@ public struct FindMyIPView: View {
         
         NavigationStack{
             
-            ZStack{
-                
-                VStack{
-                    
-                    if let response = getIpViewModel.getIpResponse{
-                        
-                        ScrollView{
-                            LazyVStack(alignment: .leading) {
-                               
-                                commonFieldView(key: "IP:", value: response.ip)
-                                
-                                commonFieldView(key: "City:", value: response.city)
-
-                                commonFieldView(key: "ASN:", value: response.asn)
-                                
-                                commonFieldView(key: "Country:", value: response.country)
-
-                                commonFieldView(key: "Country Population:", value: response.countryPopulation)
-                                
-                                commonFieldView(key: "Country Area:", value: response.countryArea)
-
-                                commonFieldView(key: "Country Calling Code:", value: response.countryCallingCode)
-                                
-                                commonFieldView(key: "Country Captial:", value: response.countryCapital)
-
-                                commonFieldView(key: "Country Name:", value: response.countryName)
-                               
-                                commonFieldView(key: "Country Languages:", value: response.languages)
-
-                                commonFieldView(key: "Latitude:", value: response.latitude)
-                                   
-                                commonFieldView(key: "Longitude:", value: response.longitude)
-
-                            }
-                            .padding()
-                            .frame(width: UIScreen.main.bounds.size.width)
-                        }
-                    }else{
-                        Text("Something Went Wrong")
-                            .font(.system(size: 15,weight: .bold))
-
-                    }
+            VStack{
+                switch getIpViewModel.state {
+                case .initial:
+                    initialView()
+                case .loading:
+                    ProgressView()
+                        .tint(Color.blue)
+                case .Sucess:
+                    ipDetailsListView
+                case .failure:
+                    errorView()
                 }
-                .navigationTitle("IP Details")
-                .navigationBarTitleDisplayMode(.large)
-                
-
             }
+            .navigationTitle("IP Details")
+            .navigationBarTitleDisplayMode(.large)
+        
         }
-        .onAppear(){
-            getIpViewModel.getIpApi()
-        }
+       
  
+    }
+    
+    var ipDetailsListView:some View{
+        
+        ZStack{
+            
+            VStack{
+                
+               
+
+                
+                if let response = getIpViewModel.getIpResponse{
+                    
+                    ScrollView{
+                        LazyVStack(alignment: .leading) {
+                            
+                            commonFieldView(key: "IP:", value: response.ip)
+                            
+                            commonFieldView(key: "City:", value: response.city)
+                            
+                            commonFieldView(key: "ASN:", value: response.asn)
+                            
+                            commonFieldView(key: "Country:", value: response.country)
+                            
+                            commonFieldView(key: "Country Population:", value: response.countryPopulation)
+                            
+                            commonFieldView(key: "Country Area:", value: response.countryArea)
+                            
+                            commonFieldView(key: "Country Calling Code:", value: response.countryCallingCode)
+                            
+                            commonFieldView(key: "Country Captial:", value: response.countryCapital)
+                            
+                            commonFieldView(key: "Country Name:", value: response.countryName)
+                            
+                            commonFieldView(key: "Country Languages:", value: response.languages)
+                            
+                            commonFieldView(key: "Latitude:", value: response.latitude)
+                            
+                            commonFieldView(key: "Longitude:", value: response.longitude)
+                            
+                        }
+                        .padding()
+                        .frame(width: UIScreen.main.bounds.size.width)
+                    }
+                    
+                }
+                
+            }
+           
+
+
+        }
+    }
+    
+    
+    func initialView() -> some View{
+        Button {
+            isButtonTapped = true
+            getIpViewModel.getIpApi()
+
+        } label: {
+            VStack{
+                Text("Get IP Details")
+                    .font(.system(size: 20,weight: .bold))
+                    .foregroundColor(Color.blue)
+                
+                ZStack{
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.blue)
+                        .frame(width: 100,height: 50)
+                    
+                    Text("Click Here")
+                        .font(.system(size: 15,weight: .medium))
+                        .foregroundColor(Color.white)
+
+                    
+                }
+            }
+        
+                
+        }
+    }
+    
+    func errorView() -> some View{
+        Text("Something went wrong")
+            .font(.system(size: 15,weight: .bold))
     }
     
     func commonFieldView<T:CustomStringConvertible>(key:String, value:T) -> some View{
